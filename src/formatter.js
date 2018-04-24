dfa7c0c4b05ee365684f17a1e627cc050b247e8a
Diego Muracciole <diegomuracciole@gmail.com>
2018-04-24 16:21:58 -0300

    Fix tabs

--- src/formatter.js
+++ src/formatter.js
@@ -5,97 +5,95 @@ import linebreak from "./linebreak";
 const SOFT_HYPHEN_HEX = '\u00ad';
 const NO_BREAK_SPACE_DECIMAL = 160;
 
-const isValidWhiteSpace = (glyphString, codePoint, index) => (
-	glyphString.isWhiteSpace(index) && codePoint !== NO_BREAK_SPACE_DECIMAL
-);
+const isValidWhiteSpace = (glyphString, codePoint, index) => (glyphString.isWhiteSpace(index) && codePoint !== NO_BREAK_SPACE_DECIMAL);
 
-const getWhiteSpaceId = glyph => (
-	glyph._font.layout(' ').glyphs[0].id
-)
+const getWhiteSpaceId = glyph => (glyph._font.layout(' ').glyphs[0].id)
 
 const getWords = glyphString => {
-	const words = [];
-	const { start } = glyphString;
-	let whiteSpaceId = null;
-	let lastIndex = 0;
+  const words = [];
+  const {start} = glyphString;
+  let whiteSpaceId = null;
+  let lastIndex = 0;
 
-	for (const { index, glyph } of glyphString) {
-		const codePoint = glyphString.codePointAtGlyphIndex(index);
+  for (const {index, glyph} of glyphString) {
+    const codePoint = glyphString.codePointAtGlyphIndex(index);
 
-		if (isValidWhiteSpace(glyphString, codePoint, index - start)) {
-			const word = glyphString.slice(lastIndex, index - start);
+    if (isValidWhiteSpace(glyphString, codePoint, index - start)) {
+      const word = glyphString.slice(lastIndex, index - start);
 
-			if (word.length > 0) {
-				words.push(word);
-			}
+      if (word.length > 0) {
+        words.push(word);
+      }
 
-			if (!whiteSpaceId) {
-				whiteSpaceId = whiteSpaceId || getWhiteSpaceId(glyph);
-			}
+      if (!whiteSpaceId) {
+        whiteSpaceId = whiteSpaceId || getWhiteSpaceId(glyph);
+      }
 
-			lastIndex = index - start + 1;
-		}
+      lastIndex = index - start + 1;
+    }
 
-		// We cache a valid whitespace glyph id to replace no-break-spaces with it
-		// after word breaking. If not present, we create it. This hack is purely
-		// for efficiency and performance purposes.
-		if (codePoint === NO_BREAK_SPACE_DECIMAL) {
-			glyph.id = whiteSpaceId;
-		}
-	}
+    // We cache a valid whitespace glyph id to replace no-break-spaces with it
+    // after word breaking. If not present, we create it. This hack is purely
+    // for efficiency and performance purposes.
+    if (codePoint === NO_BREAK_SPACE_DECIMAL) {
+      glyph.id = whiteSpaceId;
+    }
+  }
 
-	if (lastIndex < glyphString.end) {
-		const word = glyphString.slice(lastIndex, glyphString.end - glyphString.start);
-		words.push(word);
-	}
+  if (lastIndex < glyphString.end) {
+    const word = glyphString.slice(lastIndex, glyphString.end - glyphString.start);
+    words.push(word);
+  }
 
-	return words;
+  return words;
 }
 
 const h = new Hypher(english);
 const hyphenateString = (string) => {
-	if (string.includes(SOFT_HYPHEN_HEX)) {
-		return string.split(SOFT_HYPHEN_HEX)
-	}
+  if (string.includes(SOFT_HYPHEN_HEX)) {
+    return string.split(SOFT_HYPHEN_HEX)
+  }
 
-	return h.hyphenate(string);
+  return h.hyphenate(string);
 }
 
 const hyphenateWord = (glyphString) => {
-	const hyphenated = hyphenateString(glyphString.string);
+  const hyphenated = hyphenateString(glyphString.string);
 
-	let index = 0;
-	const parts = hyphenated.map(part => {
-		const res = glyphString.slice(index, index + part.length);
-		index += part.length;
-		return res;
-	});
+  let index = 0;
+  const parts = hyphenated.map(part => {
+    const res = glyphString.slice(index, index + part.length);
+    index += part.length;
+    return res;
+  });
 
-	return parts;
+  return parts;
 }
 
-const hyphenate = (words) => (
-	words.map(word => hyphenateWord(word))
-);
+const hyphenate = (words) => (words.map(word => hyphenateWord(word)));
 
 const formatter = (measureText, callback) => {
   const spaceWidth = measureText(' ');
-	const hyphenWidth = measureText('-');
-	const hyphenPenalty = 100;
-  const opts = { width: 3, stretch: 6, shrink: 9 };
+  const hyphenWidth = measureText('-');
+  const hyphenPenalty = 100;
+  const opts = {
+    width: 3,
+    stretch: 6,
+    shrink: 9
+  };
 
-  return (glyphString) => {
+  return(glyphString) => {
     const nodes = [];
     const words = getWords(glyphString);
     const spaceStretch = spaceWidth * opts.width / opts.stretch;
     const spaceShrink = spaceWidth * opts.width / opts.shrink;
-		const hyphenationCallback = callback || hyphenate;
-		const hyphenatedWords = hyphenationCallback(words, glyphString);
+    const hyphenationCallback = callback || hyphenate;
+    const hyphenatedWords = hyphenationCallback(words, glyphString);
 
     hyphenatedWords.forEach((word, index, array) => {
       if (word.length > 1) {
         word.forEach((part, partIndex, partArray) => {
-					const isLastPart = partIndex === word.length - 1;
+          const isLastPart = partIndex === word.length - 1;
 
           nodes.push(linebreak.box(measureText(part), part, !isLastPart));
 
@@ -104,7 +102,7 @@ const formatter = (measureText, callback) => {
           }
         });
       } else {
-      	nodes.push(linebreak.box(measureText(word[0]), word[0]));
+        nodes.push(linebreak.box(measureText(word[0]), word[0]));
       }
       if (index === array.length - 1) {
         nodes.push(linebreak.glue(0, linebreak.infinity, 0));
